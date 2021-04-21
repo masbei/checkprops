cd9c8ae30d6f832c9387ed995bbfbb2079f1d53f
masbei <mohamedamine.sbei@esprit.tn>
2021-04-21 02:24:59 +0200

    tst

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from './navbar/nav'
+import './App.css';
+import Profile from './profile/profile'
+
+
+
+function App() {
+  return (
+    <div classNameName="App">
+     
+    <Navbar/>
+    <br/>
+    <br/>
+   
+    <Profile fullName="mohamed amine sbei" Age="28" profession="Ingenieur Electromécanique:genie industriel" photo="https://media-exp1.licdn.com/dms/image/C4D03AQGGOv2iZXLEQA/profile-displayphoto-shrink_200_200/0/1591956574663?e=1622073600&v=beta&t=-INUcMZYuQ-uQQCoAUDOYnhic8ijm_rQrP810r87JcI"/>
+    
+
+    </div>
+  );
+}
+
+export default App;
